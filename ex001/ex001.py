e389e98aac0e2b932106d9b00b20866acc0f6187
OYanEnrique <enrique4work@gmail.com>
2026-01-19 15:38:40 -0300

    refactor: improve ex001 with docstring and magic methods

--- ex001/ex001.py
+++ ex001/ex001.py
@@ -1,23 +1,27 @@
 # Fazendo Bolinhos
 
 class Bolo:
-	def __init__(self):
-		self.nome ="bolo"
-		self.farinha = False
-		self.ovo = False
-		self.leite = False
+	"""
+	Classe para representar um bolo com ingredientes básicos.
+
+	Para fazer um bolo, é necessário ter farinha, ovo e leite.
+	Se algum desses ingredientes estiver faltando, o bolo não pode ser feito.
+	"""
+	def __init__(self, nome="", farinha=True, ovo=True, leite=True):
+		self.nome = nome
+		self.farinha = farinha
+		self.ovo = ovo
+		self.leite = leite
 		
-	def fazerBolo(self):
-		return f"Fazendo bolo {self.nome}!" if self.farinha and self.ovo and self.leite else f"Não pode fazer {self.nome}, ingredientes faltando!" 
+	def __str__(self): 
+		return f"Fazendo bolo {self.nome}!" if self.farinha and self.ovo and self.leite else f"Não pode fazer {self.nome}, ingredientes faltando!"
+		
+	def __getstate__(self):
+		return f"Nome: {self.nome}, Farinha: {self.farinha}, Ovo: {self.ovo}, Leite: {self.leite}"
 			
-bolinho = Bolo()
-bolinho.nome="Red Velvet"
-bolinho.farinha = True
-bolinho.ovo = True
-bolinho.leite = True
-print(bolinho.fazerBolo())
-
-cupcake = Bolo()
-cupcake.nome = "Cupcake"
-print(cupcake.fazerBolo())
-
+bolinho = Bolo("Red Velvet", False, True, True)
+print(bolinho)
+cupcake = Bolo("Cupcake", True, True, True)
+print(cupcake)
+print(bolinho.__doc__)
+print(cupcake.__getstate__())
